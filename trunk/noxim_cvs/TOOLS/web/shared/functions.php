b91938ec62d6cfc51b33d89de693a83d727c1a81
ffazzino <ffazzino@97b0e065-5431-6c2f-dead-21adce20b0e3>
2005-12-14 14:53:21 +0000

    Initial release!

---
+++ trunk/noxim_cvs/TOOLS/web/shared/functions.php
@@ -0,0 +1,48 @@
+<?php
+
+/***************************/
+/* function print_header() */
+/***************************/
+function print_header() {
+?>
+<!DOCTYPE HTML SYSTEM>
+<html lang='en'>
+
+<head>
+<title>Noxim - the NoC Simulator</title>
+<meta http-equiv='Content-Type' content='text/html; charset=ISO-8859-1'>
+<meta http-equiv='Content-Language' content='en'>
+<meta http-equiv='Content-Style-Type' content='text/css'>
+<meta http-equiv='Content-Script-Type' content='text/javascript'>
+<meta name='Description' content='Noxim - the NoC Simulator; this is the web interface of Noxim, a simulator of Network-on-Chips (NoCs) writtem in SystemC'>
+<meta name='Keywords' content='noxim, noc, simulator, network-on-chip, catania, systemc, router, mesh, tile, dyad, odd-even, xy, routing, wormhole, flit'>
+<meta name="Author" content="Fabrizio Fazzino">
+<meta name="Reply-To" content="fabrizio@fazzino.it">
+<meta name="Copyright" content="(C) 2005 University of Catania">
+<meta name="Generator" content="Just my fingertips">
+<meta name="Robots" content="INDEX, FOLLOW">
+<link rel="shortcut icon" href="images/noxicon.ico">
+<link rel='stylesheet' type='text/css' href='shared/style.css'>
+</head>
+
+<body>
+<div align="center">
+<h1>Noxim</h1>
+<h2>the NoC Simulator</h2>
+<?php
+}
+
+
+/***************************/
+/* function print_footer() */
+/***************************/
+function print_footer() {
+?>
+<p class="subTitle">&copy; 2005 Universit&agrave; di Catania</p>
+</div>
+</body>
+</html>
+<?php
+}
+
+?>
