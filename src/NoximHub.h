cdefe643a48c146a27288bc70666c63eee9fd218
xedivad@gmail.com <xedivad@gmail.com@97b0e065-5431-6c2f-dead-21adce20b0e3>
2014-09-25 12:02:29 +0000

    Completed hub cabling

--- src/NoximHub.h
+++ src/NoximHub.h
@@ -13,6 +13,8 @@
 
 #include <systemc.h>
 #include "NoximMain.h"
+#include "NoximBuffer.h"
+#include "NoximReservationTable.h"
 using namespace std;
 
 SC_MODULE(NoximHub)
@@ -31,13 +33,35 @@ SC_MODULE(NoximHub)
     sc_in <bool> ack_tx[MAX_HUB_PORTS];	        // The outgoing ack signals associated with the output channels
 
 
+    int local_id;		                // Unique ID
+    NoximBuffer buffer[MAX_HUB_PORTS];	        // Buffer for each port
+    bool current_level_rx[MAX_HUB_PORTS];	// Current level for ABP
+    bool current_level_tx[MAX_HUB_PORTS];	// Current level for ABP
+
+    int start_from_port;	                // Port from which to start the reservation cycle
+
+    // TODO: use different table or extend in order to support also
+    // Hubs
+    NoximReservationTable reservation_table;	// Switch reservation table
+
+    void rxProcess();		// The receiving process
+    void txProcess();		// The transmitting process
+
+
+
     void setup();
 
 
     // Constructor
 
     SC_CTOR(NoximHub) {
+	SC_METHOD(rxProcess);
+	sensitive << reset;
+	sensitive << clock.pos();
 
+	SC_METHOD(txProcess);
+	sensitive << reset;
+	sensitive << clock.pos();
 
     }
 
