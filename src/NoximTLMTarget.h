397add225b5d7869c97c8c1b5c21f5fed5bf7b19
xedivad@gmail.com <xedivad@gmail.com@97b0e065-5431-6c2f-dead-21adce20b0e3>
2014-09-25 13:17:22 +0000

    First TLM transmission draft

---
+++ src/NoximTLMTarget.h
@@ -0,0 +1,65 @@
+#ifndef __NOXIMTLMTARGET_H__
+#define __NOXIMTLMTARGET_H__
+
+// Needed for the simple_target_socket
+#define SC_INCLUDE_DYNAMIC_PROCESSES
+
+#include "tlm.h"
+#include "tlm_utils/simple_initiator_socket.h"
+#include "tlm_utils/simple_target_socket.h"
+
+#include <systemc.h>
+
+// Target module representing a simple memory
+
+struct Memory: sc_module
+{
+  // TLM-2 socket, defaults to 32-bits wide, base protocol
+  tlm_utils::simple_target_socket<Memory> socket;
+
+  enum { SIZE = 256 };
+
+  SC_CTOR(Memory)
+  : socket("socket")
+  {
+    // Register callback for incoming b_transport interface method call
+    socket.register_b_transport(this, &Memory::b_transport);
+
+    // Initialize memory with random data
+    for (int i = 0; i < SIZE; i++)
+      mem[i] = 0xAA000000 | (rand() % 256);
+  }
+
+  // TLM-2 blocking transport method
+  virtual void b_transport( tlm::tlm_generic_payload& trans, sc_time& delay )
+  {
+    tlm::tlm_command cmd = trans.get_command();
+    sc_dt::uint64    adr = trans.get_address() / 4;
+    unsigned char*   ptr = trans.get_data_ptr();
+    unsigned int     len = trans.get_data_length();
+    unsigned char*   byt = trans.get_byte_enable_ptr();
+    unsigned int     wid = trans.get_streaming_width();
+
+    // Obliged to check address range and check for unsupported features,
+    //   i.e. byte enables, streaming, and bursts
+    // Can ignore DMI hint and extensions
+    // Using the SystemC report handler is an acceptable way of signalling an error
+
+    if (adr >= sc_dt::uint64(SIZE) || byt != 0 || len > 4 || wid < len)
+      SC_REPORT_ERROR("TLM-2", "Target does not support given generic payload transaction");
+
+    // Obliged to implement read and write commands
+    if ( cmd == tlm::TLM_READ_COMMAND )
+      memcpy(ptr, &mem[adr], len);
+    else if ( cmd == tlm::TLM_WRITE_COMMAND )
+      memcpy(&mem[adr], ptr, len);
+
+    // Obliged to set response status to indicate successful completion
+    trans.set_response_status( tlm::TLM_OK_RESPONSE );
+  }
+
+  int mem[SIZE];
+};
+
+
+#endif
