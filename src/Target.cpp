94b26f4417f2d7f94db71edded60903dd9dc7ec0
xedivad@gmail.com <xedivad@gmail.com@97b0e065-5431-6c2f-dead-21adce20b0e3>
2014-12-01 10:57:05 +0000

    Implemented working Flit transmission

--- src/Target.cpp
+++ src/Target.cpp
@@ -9,6 +9,7 @@ void Target::b_transport( tlm::tlm_generic_payload& trans, sc_time& delay )
     unsigned char*   byt = trans.get_byte_enable_ptr();
     //unsigned int     wid = trans.get_streaming_width();
 
+    cout << name() << "::b_transport() received data with size " << len << endl;
     // Obliged to check address range and check for unsupported features,
     //   i.e. byte enables, streaming, and bursts
     // Can ignore DMI hint and extensions
@@ -24,38 +25,16 @@ void Target::b_transport( tlm::tlm_generic_payload& trans, sc_time& delay )
     else if ( cmd == tlm::TLM_WRITE_COMMAND )
 	memcpy(&mem[adr], ptr, len);
 
-    cout << name() << " Time: " << sc_time_stamp()  << " >>>> Target received " <<  mem[0] << endl;
+    Flit * my_flit;
+    my_flit = (Flit*)(&mem[0]);
 
-    Flit f = get_payload();
+    //cout << name() << " Time: " << sc_time_stamp()  << " >>>> Target received " <<  mem[0] << endl;
+    cout << name() << " Time: " << sc_time_stamp()  << " >>>> Target received Flit , Type " << my_flit->flit_type << ", " << my_flit->src_id << "-->" << my_flit->dst_id << " flit: " << *my_flit << endl;
+    //Flit f = get_payload();
 
-    buffer_rx.Push(f);
+    buffer_rx.Push(*my_flit);
 
     // Obliged to set response status to indicate successful completion
     trans.set_response_status( tlm::TLM_OK_RESPONSE );
-}
-
-Flit Target::get_payload()
-{
-    static int seq = 0;
-
-
-    Flit f;
-    cout << name() << "::get_payload() returning fake flit with src_id = 0, seq " << seq << endl;
-    f.src_id = 0;
-    f.sequence_no = seq;
-
-    // seed 0, flit HBBT
-    if (seq==0)
-	f.flit_type = FLIT_TYPE_HEAD;
-    else if (seq==3)
-	f.flit_type = FLIT_TYPE_TAIL;
-    else
-	f.flit_type = FLIT_TYPE_BODY;
-
-    seq++;
-
-    if (seq==4) seq = 0;
-
-    return f;
 }
 
