f6d420ecc4d45ced03450fc3ad88be464bed4a0d
salvatoremonteleone@gmail.com <salvatoremonteleone@gmail.com@97b0e065-5431-6c2f-dead-21adce20b0e3>
2015-01-12 16:36:42 +0000

    Added parameters to run_tests.sh to run selected tests

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-mkdir -p out_tt
+OUT_DIR=out_tt
+IN_DIR=tt
+FILE_PREFIX=tt_
+FILE_POSTFIX=.txt
 
-for f in tt/*
-    do ./noxim -seed 0 -winoc -traffic table $f > out_$f
-done
+mkdir -p $OUT_DIR
+
+if [ $# -eq 0 ]
+then
+    for F in $IN_DIR/*
+    do
+        echo "Traffic Table" $F "is:"
+        cat "$F"
+
+        ./noxim -seed 0 -winoc -traffic table "$F" > "$OUT_DIR/$(basename $F)"
+        echo
+    done
+else
+    for VAR in "$@"
+    do
+        FILENAME=$FILE_PREFIX$VAR$FILE_POSTFIX
+        echo "Traffic Table" $FILENAME "is:"
+        cat "$IN_DIR/$FILENAME"
+        ./noxim -seed 0 -winoc -traffic table "$IN_DIR/$FILENAME" > "$OUT_DIR/$FILENAME"
+        echo
+    done
+fi
