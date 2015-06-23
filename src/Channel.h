991e86ef512430bf90cff79d030f0ea8faf14e00
Davide Patti <xedivad@gmail.com>
2015-06-23 14:31:50 +0200

    Enable cycle delay declaration

--- src/Channel.h
+++ src/Channel.h
@@ -54,7 +54,7 @@ struct Channel: sc_module
     flit_transmission_delay_ps = 1000*GlobalParams::flit_size/GlobalParams::channel_configuration[local_id].dataRate;
     flit_transmission_cycles = ceil(((double)flit_transmission_delay_ps/GlobalParams::clock_period_ps));
 
-    //LOG << "data rate " << GlobalParams::channel_configuration[local_id].dataRate << " Gbps, transmission delay " << flit_transmission_delay_ps << " ps, " << flit_transmission_cycles << " cycles " << endl; 
+    LOG << "data rate " << GlobalParams::channel_configuration[local_id].dataRate << " Gbps, transmission delay " << flit_transmission_delay_ps << " ps, " << flit_transmission_cycles << " cycles " << endl; 
 
   }
 
