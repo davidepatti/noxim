5b1fec923c545074b2f964f98423c20c4b013ae6
Salvatore Monteleone <salvatoremonteleone@gmail.com>
2015-07-07 18:22:00 +0200

    Updated script to work in Ubuntu LTS (from 10.04)

--- doc/install_script/install_ubuntu.sh
+++ doc/install_script/install_ubuntu.sh
@@ -1,12 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 sudo apt-get update
-sudo apt-get install build-essential linux-headers-$(uname -r) wget tar git libyaml-cpp-dev libboost-dev
-cd
+sudo apt-get install build-essential linux-headers-$(uname -r) wget tar 
+sudo apt-get install git || \
+    sudo apt-get install python-software-properties && \
+    sudo add-apt-repository ppa:git-core/ppa && \
+    sudo apt-get update && \
+    sudo apt-get install git
+sudo apt-get install libboost-dev
+sudo apt-get install libyaml-cpp-dev || \
+    sudo apt-get install cmake && \
+    git clone https://github.com/jbeder/yaml-cpp && \
+    cd yaml-cpp/ && \
+    mkdir lib && \
+    cd lib && \
+    cmake .. && \
+    make && \
+    sudo make install
+
+cd ../..
+
 wget http://www.accellera.org/images/downloads/standards/systemc/systemc-2.3.1.tgz
 tar -xzf systemc-2.3.1.tgz
 cd systemc-2.3.1
 mkdir objdir
 cd objdir
+export CXX=g++
+export CC=gcc
 sudo mkdir /usr/local/systemc-2.3.1
 ../configure --prefix=/usr/local/systemc-2.3.1
 
@@ -17,7 +36,7 @@ echo `echo /usr/local/systemc-2.3.1/lib-*` > systemc.conf
 sudo mv systemc.conf /etc/ld.so.conf.d/
 sudo ldconfig
 
-cd 
+cd ../..
 
 git clone https://github.com/davidepatti/noxim
 
