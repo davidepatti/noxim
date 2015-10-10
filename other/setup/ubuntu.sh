f62daa318ea7014f013664d083ee36895b138160
Salvatore Monteleone <salvatoremonteleone@gmail.com>
2015-10-10 20:56:18 +0200

    Changes to load libraries from noxim/bin/libs

--- other/setup/ubuntu.sh
+++ other/setup/ubuntu.sh
@@ -2,51 +2,46 @@
 export GIT_SSL_NO_VERIFY=1
 
 sudo apt-get update
-sudo apt-get -y install build-essential linux-headers-$(uname -r) wget tar 
-sudo apt-get -y install git || \
-    sudo apt-get -y install python-software-properties && \
-    sudo add-apt-repository ppa:git-core/ppa && \
-    sudo apt-get update && \
-    sudo apt-get -y install git
-sudo apt-get -y install libboost-dev
+sudo apt-get -y install build-essential linux-headers-$(uname -r) wget tar libboost-dev cmake 
 
-sudo apt-get -y install libyaml-cpp-dev || \
-    if [ $(dpkg -s libyaml-cpp-dev 2>/dev/null | grep "ok installed" | wc -l) -eq 0 ]
-    then
-        sudo apt-get -y install cmake && \
-        git clone https://github.com/jbeder/yaml-cpp && \
-        cd yaml-cpp/ && \
-        mkdir lib && \
-        cd lib && \
-        cmake .. && \
-        make && \
-        sudo make install
-        cd ../..
-    fi
+if [ $(dpkg -s git 2>/dev/null | grep "ok installed" | wc -l) -eq 0 ]
+then 
+    sudo apt-get -y install git || \
+        sudo apt-get -y install python-software-properties && \
+        sudo add-apt-repository ppa:git-core/ppa && \
+        sudo apt-get update && \
+        sudo apt-get -y install git
+fi
+
+git clone https://github.com/davidepatti/noxim
+cd noxim/bin
+mkdir -p libs
+cd libs
+
+git clone https://github.com/jbeder/yaml-cpp
+cd yaml-cpp/
+mkdir -p lib
+cd lib
+cmake ..
+make
+cd ../..
 
 wget http://www.accellera.org/images/downloads/standards/systemc/systemc-2.3.1.tgz
 tar -xzf systemc-2.3.1.tgz
 cd systemc-2.3.1
-mkdir objdir
+mkdir -p objdir
 cd objdir
 export CXX=g++
 export CC=gcc
-sudo mkdir /usr/local/systemc-2.3.1
-../configure --prefix=/usr/local/systemc-2.3.1
-
+../configure 
 make
-sudo make install
-
-echo `echo /usr/local/systemc-2.3.1/lib-*` > systemc.conf
+make install
+cd ..
+echo `pwd`/lib-* > systemc.conf
 sudo mv systemc.conf /etc/ld.so.conf.d/
 sudo ldconfig
-
 cd ../..
-
-git clone https://github.com/davidepatti/noxim
-
-cd noxim/bin
 
 make
 
-./noxim -config ../config_examples/default-config.yaml .
+./noxim -config ../config_examples/default_config.yaml
