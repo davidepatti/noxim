937d86e30efcf49cef3ef1243e65b09ad7656ec7
Davide Patti <xedivad@gmail.com>
2016-12-14 10:46:38 +0100

    Fixed negative first (thanks to amircharif)

--- src/routingAlgorithms/Routing_NEGATIVE_FIRST.cpp
+++ src/routingAlgorithms/Routing_NEGATIVE_FIRST.cpp
@@ -17,30 +17,28 @@ vector<int> Routing_NEGATIVE_FIRST::route(Router * router, const RouteData & rou
     Coord current = id2Coord(routeData.current_id);
     Coord destination = id2Coord(routeData.dst_id);
     vector <int> directions;
+    
+    // Negative directions:
+    // WEST (current x > dest x)
+    // SOUTH (current y > dest y)
+    //
+    // Algorithm: Never switch from a positive direction to a negative
+    // So, any south or west direction should always have priority
 
-    if ((destination.x <= current.x && destination.y <= current.y) ||
-            (destination.x >= current.x && destination.y >= current.y))
+    if (destination.x < current.x || destination.y > current.y) // check negative directions first
     {
-        if(!xy)
-        {
-            xy = RoutingAlgorithms::get("XY");
-            
-            if (!xy)
-                assert(false);
-        }
-
-        return xy->route(router, routeData);
-    }
-    if (destination.x > current.x && destination.y < current.y) 
-    {
-        directions.push_back(DIRECTION_NORTH);
-        directions.push_back(DIRECTION_EAST);
-    }
-    else 
-    {
-        directions.push_back(DIRECTION_SOUTH);
-        directions.push_back(DIRECTION_WEST);
-    }
+	// note: one or both negative directions could be added
+	if (destination.x < current.x) directions.push_back(DIRECTION_WEST);
+	if (destination.y > current.y) directions.push_back(DIRECTION_SOUTH);
+    } 
+    else  // no negative direction to process, check if positive ones are needed
+	if (destination.x > current.x || destination.y < current.y) 
+	{
+	    if (destination.x > current.x) directions.push_back(DIRECTION_EAST);
+	    if (destination.y < current.y) directions.push_back(DIRECTION_NORTH);
+	} 
+	else // both x and y were already reached
+	    directions.push_back(DIRECTION_LOCAL);
 
     return directions;
 }
